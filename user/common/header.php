16c4bba21f941275da8f85a2c40713aa749f159c
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-06-24 18:06:21 +0800

    Header design

--- user/common/header.php
+++ user/common/header.php
@@ -1,3 +1,11 @@
 <div class="header">
-    Coder老混混
+    <div class="container-full">
+        <div class="sitename">
+            Coder老混混
+        </div>
+        <div class="userinfo">
+            <span class="username">用戶張三</span>
+            <a href="/user/logout.php">退出</a>
+        </div>
+    </div>
 </div>
