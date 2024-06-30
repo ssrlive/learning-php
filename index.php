d973d9d95878cb94a9ef75f1f0f3c0b7bdc5091f
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-06-30 23:45:50 +0800

    index page

--- index.php
+++ index.php
@@ -3,4 +3,9 @@ if (version_compare(PHP_VERSION, '7.4', '<')) {
     die('This Application requires PHP version 7.4 or higher.');
 }
 
-echo "Hello World!";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/func/Func.php";
+if (isSessionLoggedIn()) {
+    header("Location: /user/usercenter.php");
+} else {
+    header("Location: /user/login.php");
+}
